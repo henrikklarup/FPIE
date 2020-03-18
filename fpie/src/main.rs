b0a0194325f8c28799ae269f7334fa01f188070d
Henrik Klarup <hkl@siteimprove.com>
2020-03-18 21:24:34 +0100

    Add tar packing

--- fpie/src/main.rs
+++ fpie/src/main.rs
@@ -1,5 +1,9 @@
-extern crate clap; 
-use clap::{App, Arg}; 
+extern crate clap;
+use clap::{App, Arg};
+
+extern crate tar;
+use tar::Builder;
+use std::fs::File;
 
 mod utils;
 
@@ -21,13 +25,43 @@ fn main() {
         .value_name("PATH")
         .help("Specify the context path")
         .takes_value(true).required(true))
+        .arg(Arg::with_name("output")
+        .short("o")
+        .long("output")
+        .value_name("PATH")
+        .help("Specify output file path")
+        .takes_value(true))
+        .arg(Arg::with_name("dry-run")
+        .short("d")
+        .help("Print a list of files to be packed"))
        .get_matches();
 
     let inc = matches.value_of("includefile").unwrap();
     let context = matches.value_of("context").unwrap();
-    let final_list = do_it(inc, context);
+    let mut context_path_with_trailing_slash = context.to_string(); 
+    if !context.ends_with("/") {
+        context_path_with_trailing_slash.push_str("/");
+    }
+    let dry_run = matches.is_present("dry-run");
+    let final_list = do_it(inc, &context_path_with_trailing_slash);
+    if dry_run {
+        for itm in final_list {
+            println!("{}", itm);
+        }
+        return;
+    }
+
+    let mut output_file = "output.tar";
+    let output_val = matches.value_of("output");
+    if let Some(output) = output_val {
+        output_file = output;
+    }
+
+    let file = File::create(output_file).unwrap();
+    let mut a = Builder::new(file);
+
     for itm in final_list {
-        println!("{}", itm);
+        a.append_path_with_name(format!("{}{}", context_path_with_trailing_slash, itm), itm).unwrap();
     }
 }
 
@@ -39,13 +73,8 @@ pub fn do_it(includefile_path: &str, context_path: &str) -> Vec<String> {
     let excludedfiles = utils::expand_globs_to_files(context_path, excludes);
 
     let list_with_context_path = utils::except(includedfiles, excludedfiles);
-
-    let mut context_path_with_trailing_slash = context_path.to_string(); 
-    if !context_path.ends_with("/") {
-        context_path_with_trailing_slash.push_str("/");
-    }
     
-    let final_list = utils::trim_context(list_with_context_path, &context_path_with_trailing_slash);
+    let final_list = utils::trim_context(list_with_context_path, &context_path);
     return final_list;
 }
 
